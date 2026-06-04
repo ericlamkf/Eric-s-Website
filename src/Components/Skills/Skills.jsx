0a9df1eb1a7b5e2312617ec4ce15e3aaff574315
Eric Lam Kah Fai <linj361@gmail.com>
2026-06-04 13:39:36 +0800

    feat: add Skills component with interactive category filtering and progress bars

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -1,28 +1,67 @@
-import React from 'react'
-import { useRef } from 'react'
-import { motion, useScroll, useTransform, useMotionValueEvent } from 'framer-motion'
+import React, { useState } from 'react'
 import './Skills.css'
-import brain from '../../assets/digital brain.png'
+
+const skills = [
+    // Frontend
+    { name: "HTML5/CSS", level: 95, category: "frontend" },
+    { name: "JavaScript/TypeScript", level: 90, category: "frontend" },
+    { name: "React", level: 80, category: "frontend" },
+    { name: "Tailwind CSS", level: 70, category: "frontend" },
+    { name: "XML", level: 90, category: "frontend" },
+
+    // Backend
+    { name: "Python", level: 90, category: "backend" },
+    { name: "Java", level: 95, category: "backend" },
+    { name: "Spring Boot", level: 60, category: "backend" },
+    { name: "Firebase", level: 80, category: "backend" },
+    { name: "Supabase", level: 80, category: "backend" },
+    { name: "C", level: 80, category: "backend" },
+
+    // Tool
+    { name: "Git/GitHub", level: 90, category: "tools" },
+    { name: "Figma", level: 90, category: "tools" },
+    { name: "Android Studio", level: 90, category: "tools" },
+    { name: "Postman", level: 90, category: "tools" },
+    { name: "Google Colab", level: 90, category: "tools" },
+
+];
+
+const categories = ["all", "frontend", "backend", "tools"]
 
 const Skills = () => {
-    const targetRef = useRef(null)
-    const { scrollYProgress } = useScroll({
-        target: targetRef,
-        offset: ["start end", "end start"]
-    });
-
-    const scale = useTransform(scrollYProgress, [0,0.8], [0.5,1]);
+    const [currentCategory, setCurrentCategory] = useState("all");
+    const filteredSkills = skills.filter((skill) => currentCategory === "all" || skill.category === currentCategory)
 
     return (
         <div className='skills'>
-            <div className="designer">
-                <h1 className='rgbText'>Designer <i class="fa-solid fa-pen-ruler"></i></h1>
-                <p>As a designer, I bring a blend of creativity, precision, and innovation to every project. My approach is rooted in crafting visually stunning and user-centered designs that balance functionality with aesthetics.</p>
+            <div className="category">
+                {categories.map((category, key) => (
+                    <button key={key} onClick={() => setCurrentCategory(category)} className={'category-btn ' + (category === currentCategory ? "active" : "")}>
+                        {category.toUpperCase()}
+                    </button>
+                ))}
             </div>
-            <motion.img ref={targetRef} style={{ scale }} src={brain} />
-            <div className="coder">
-                <h1 className="rgbText">Coder <i class="fa-solid fa-keyboard"></i></h1>
-                <p>As a coder, I thrive on transforming complex problems into elegant, efficient solutions. Combining logical precision with innovative thinking, I craft clean and scalable code that drives functionality and performance.</p>
+
+            <div className="skills-container">
+                {filteredSkills.map((skill, key) => (
+                    <div className="skill-card">
+                        {/* Skill Name */}
+                        <div className="skill-name">
+                            <h3>{skill.name}</h3>
+                        </div>
+
+                        {/* Skill Level */}
+                        <div className="skill-level">
+                            <div className="skill-progress" style={{ width: `${skill.level}%` }}></div>
+                        </div>
+
+                        {/* Percentage */}
+                        <div className="skill-percent">
+                            {skill.level}%
+                        </div>
+                    </div>
+                ))
+                }
             </div>
         </div>
     )
